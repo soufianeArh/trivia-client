4e19a2103badbbe7a6b0f023d3406f6347ccff1c
Jannis Seemann <5215310+jannis-seemann@users.noreply.github.com>
2024-05-10 13:02:05 +0300

    first commit

---
+++ src/components/Quiz.tsx
@@ -0,0 +1,29 @@
+import {useEffect, useState} from "react";
+import superagent from "superagent";
+
+export default function Quiz() {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [error, setError] = useState(false);
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() => {
+        if (isLoading) return;
+        setIsLoading(true);
+
+        superagent.get("http://localhost:3000/api/questions")
+            .set('accept', 'json')
+            .then((res) => {
+                console.log(res)
+                setQuestions(res.body);
+            })
+            .catch((err: any) => setError(err));
+    })
+
+
+    return (
+        <>
+            <pre style={{whiteSpace: 'wrap', fontSize: 16}}>{JSON.stringify(questions)}</pre>
+        </>
+    )
+}
